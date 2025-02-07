fd5146179e3dcb6db34a1ea2a096b5459e88b371
Roméo Giorgio <giorgio.romeo95@gmail.com>
2025-02-07 09:59:55 +0100

    check todo

--- src/List/4-template/List/List.tsx
+++ src/List/4-template/List/List.tsx
@@ -1,7 +1,8 @@
 //********** Imports **********//
 import { TODO } from "../../../utils/types";
+import DoneIcon from "@mui/icons-material/Done";
 
-import { useSelector } from "../../../store";
+import { useDispatch, useSelector } from "../../../store";
 import {
   Box,
   Divider,
@@ -9,22 +10,31 @@ import {
   ListItemButton,
   ListItemText,
 } from "@mui/material";
-import { todoGlobalizedSelector } from "../../../redux/todoSlice";
+import { todoGlobalizedSelector, updateTodo } from "../../../redux/todoSlice";
+import { useState } from "react";
 
 //********** Component **********//
 const ListComponent = () => {
   const todos = useSelector((state) =>
     todoGlobalizedSelector.selectAll(state.todos)
   );
-  console.log(todos);
+  const dispatch = useDispatch();
+  const handleTODOClick = (id: number) => {
+    dispatch(updateTodo({ id, changes: { done: true } }));
+  };
   return (
     <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       {todos.map((todo: TODO) => (
         <>
           <ListItem disablePadding id={todo.id.toString()} key={Math.random()}>
-            <ListItemButton>
+            <ListItemButton
+              onClick={() => {
+                handleTODOClick(todo.id);
+              }}
+            >
               <ListItemText primary={todo.text} />
             </ListItemButton>
+            {todo?.done ? <DoneIcon /> : null}
           </ListItem>
           <Divider />
         </>
